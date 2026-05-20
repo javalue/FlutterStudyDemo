e0c51d17fa00a8aadab495ce245fb74a265454d0
tong.xu <tong.xu@quvideo.com>
2026-05-20 17:11:45 +0800

    feat:保持父组件的简洁性

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
+class CounterDisplay extends StatelessWidget {
+  const CounterDisplay({required this.count, super.key});
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  const CounterIncrementor({required this.onPressed, super.key});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: onPressed, child: const Text('Increment'));
+  }
+}
+
 class Counter extends StatefulWidget {
   const Counter({super.key});
 
@@ -21,9 +43,9 @@ class _CounterState extends State<Counter> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        ElevatedButton(onPressed: _increment, child: const Text("Increment")),
+        CounterIncrementor(onPressed: _increment),
         const SizedBox(width: 16),
-        Text('Count: $_counter'),
+        CounterDisplay(count: _counter),
       ],
     );
   }
